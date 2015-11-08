27f659b934e2cc40f80b264677fdfafa0f67f115
avdim <avdim@mail.ru>
2015-11-08 23:19:13 +0300

    template

--- web/index.php
+++ web/index.php
@@ -1,28 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Heroku Multipack Test</title>
+    <title>?????????.com</title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script>
+        $(window).scroll(function(){
+            var obj = $('#header');
+            $('#header-inner').toggleClass('scrolling', $(window).scrollTop() > 150);
+            $('#temp').toggleClass('invisible', $(window).scrollTop() <= 150)
+        });
+    </script>
+    <style type="text/css">
+        html, body, span, object, iframe, h1, h2, h3, h4, h5, h6, a, p, blockquote, pre, abbr, address, cite, code, del, dfn, em, img, ins, kbd, q, samp, small, strong, sub, var, b, i, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, figcaption, figure, footer, header, menu, nav, section, summary, time, mark, audio, video {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            outline: 0;
+            font-size: 100%;
+            vertical-align: baseline;
+            background: transparent;
+        }
+        html {
+            height: 100%;
+        }
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+        }
 
-    <!-- Bootstrap -->
-    <link href="vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        body {
+            background-image: url("bg1.jpg");
+            background-repeat: repeat;
+        }
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body class="container">
-    <h1>Hello, Dima! <small>PHP + Node.js on Heroku</small></h1>
-    
-    <p>Running <strong class="text-success">PHP/<?=PHP_VERSION?></strong> on Heroku using the <a href="https://github.com/heroku/heroku-buildpack-php">standard PHP buildpack</a>, serving a site using <a href="http://getbootstrap.com">Bootstrap</a> which was downloaded by <a href="http://bower.io">Bower</a>, which in turn was installed by the <a href="https://github.com/heroku/heroku-buildpack-nodejs">standard Node.js buildpack</a>.</p>
-    
-    <p>Both put together using <a href="https://github.com/heroku/heroku-buildpack-multi">heroku/heroku-buildpack-multi</a>.</p>
-    
-    <p>Source: <a href="https://github.com/dzuelke/heroku-multipack-nodejs-php-example">dzuelke/heroku-multipack-nodejs-php-example</a>
-  </body>
+        .scrolling {
+            position: fixed;
+            top: 0;
+        }
+        .invisible {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+<div style="width: 100%; background-color: antiquewhite; height: 150px"></div>
+<div id="temp" class="invisible" style="height: 100px"></div>
+<div id="header-inner" style="width: 100%; background-color: deeppink; height: 100px;"></div>
+<div align="center" style="width:700px; height: 1500px; background-color: aliceblue; margin-left: auto; margin-right: auto">
+    1<br/>2<br/>1<br/>2<br/>1<br/>2<br/>1<br/>2<br/>
+</div>
+<div style="position: fixed; bottom: 0; right: 0; width: 100px; height: 100px; background-color: coral"></div>
+</body>
 </html>
