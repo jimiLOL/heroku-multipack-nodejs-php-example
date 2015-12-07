3877cfb9ed02fe0d967c883f858a86a8f5fe0402
avdim <avdim@mail.ru>
2015-12-07 20:24:39 +0300

    vk widget

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
     <title>Звездочка.com</title>
 <!--    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>-->
@@ -14,7 +14,7 @@
             outline: 0;
             font-size: 100%;
             vertical-align: baseline;
-            background: transparent;
+            /*background: transparent;*/
         }
         html {
             height: 100%;
@@ -27,14 +27,6 @@
         body {
             background-image: url("static/bg.jpg");
             background-repeat: repeat;
-        }
-
-        .scrolling {
-            position: fixed;
-            top: 0;
-        }
-        .invisible {
-            display: none;
         }
 
         .button_container {
@@ -63,35 +55,14 @@
             display: none;
         }
     </style>
+    <script type="text/javascript" src="//vk.com/js/api/openapi.js?121"></script>
+<!--    https://vk.com/dev/Community?height=400&link=http%3A%2F%2Fvk.com%2Fpublic20003922&mode=0&oid=-20003922&wide=&width=220-->
     <script>
-//        $(window).scroll(function(){
-//            var topHeight = document.getElementById("top").offsetHeight;//.clientHeight
-//            $('#button_bar').toggleClass('scrolling', $(window).scrollTop() > topHeight);
-//            document.getElementById("temp").style.display = $(window).scrollTop() <= topHeight ? "none" : "block";
-//        });
 
     </script>
 </head>
 <body>
 <div id="top" align="center" style="width: 100%; background-color: antiquewhite;"><img src="static/title.jpg"/></div>
-<div id="temp" style="display: none" class="button_container">
-    <a class="btn">
-        <img src="static/news_btn_ru.png" class="not_selected">
-        <img src="static/news_btn_ru_selected.png" class="selected">
-    </a>
-    <a target="game_info_content" class="btn">
-        <img src="static/faq_btn_ru.png" class="not_selected"/>
-        <img src="static/faq_btn_ru_selected.png" class="selected"/>
-    </a>
-    <a target="_blank" class="btn">
-        <img src="static/group_btn_ru.png" class="not_selected"/>
-        <img src="static/group_btn_ru_selected.png" class="selected"/>
-    </a>
-    <a target="game_info_content" class="btn">
-        <img src="static/help_btn_ru.png" class="not_selected"/>
-        <img src="static/help_btn_ru_selected.png" class="selected"/>
-    </a>
-</div>
 <div class="button_container" id="button_bar">
     <a class="btn">
         <img src="static/news_btn_ru.png" class="not_selected">
@@ -113,7 +84,13 @@
 <div align="center" style="width:700px; height: 1500px; background-image: url('static/text_bg.png');background-repeat: repeat; margin-left: auto; margin-right: auto">
     Сайт временно не работает.<br/>
     Приносим извинения за временные неудобства.
+
+    <div id="vk_groups"></div>
+    <script type="text/javascript">
+        VK.Widgets.Group("vk_groups", {mode: 2, width: "auto", height: "300", wide: 1}, 102947277);
+    </script>
 </div>
-<div style="position: fixed; bottom: 0; right: 0; width: 100px; height: 100px; background-color: coral"></div>
+
+<div style="position: fixed; top: 0; right: 0; width: 100px; height: 100px; background-color: coral"></div>
 </body>
 </html>
