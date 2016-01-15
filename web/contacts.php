6cbdf9e0e80bc43bc2cc81e6fbfc38aca80590bc
avdim <avdim@mail.ru>
2016-01-15 06:59:30 +0300

    contacts

--- web/contacts.php
+++ web/contacts.php
@@ -1,9 +1,35 @@
+<style type="text/css">
+    td {
+        vertical-align: middle;
+        margin-left: 5px;
+        border-left: 5px;
+        padding-left: 10px;
+    }
+</style>
 <h1>Контакты</h1>
-<img style="height: 80px;" src="static/call_us.png">
-+7 (977) 822-11-21
-<br/>
-<img style="height: 80px;" src="static/vk_top_right.png"/>
-vk.com/littlestarkids
-<br/>
-<img style="height: 80px;" src="static/mail_us.png"/>
-littlestarkids@mail.ru
+<table>
+    <tr>
+        <td>
+            <img style="height: 80px;" src="static/call_us.png">
+        </td>
+        <td>
+            +7 (977) 822-11-21
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img style="height: 80px;" src="static/vk_top_right.png"/>
+        </td>
+        <td>
+            <a href="http://vk.com/littlestarkids">vk.com/littlestarkids</a>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <img style="height: 80px;" src="static/mail_us.png"/>
+        </td>
+        <td>
+            littlestarkids@mail.ru
+        </td>
+    </tr>
+</table>
