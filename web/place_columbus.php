860c2108885c9a17a08478aafbca409061cca5a0
avdim <avdim@mail.ru>
2016-03-06 21:45:43 +0300

    photos

--- web/place_columbus.php
+++ web/place_columbus.php
@@ -1,1 +1,29 @@
-Колумбус
+<h1>Columbus</h1>
+<p>
+    Детская развлекательная площадка Little Star расположена на 3 уровне ТЦ Columbus в непосредственной близости от станции метро Пражская.
+</p>
+<p>
+    Адрес: Москва, ул. Кировоградская, д. 13.<br/>
+    Работаем в режиме торгового центра.
+</p>
+<p>
+    Это идеальное место для малышей и зимой и летом: стремительные спуски с горок в бассейн, наполненный разноцветными шариками, катание на дельфинах перенесут в волшебный мир. Мир, в котором нет ничего невозможного. Мир, где сбываются все детские мечты!
+</p>
+
+<?php require_once "places_text_bottom.php"?>
+
+<a href="columbus_1.html">
+    <img style="width: 30%" src="static/new/columbus/1.jpg"/>
+</a>
+
+<img style="width: 30%" src="static/new/columbus/2.jpg"/>
+<img style="width: 30%" src="static/new/columbus/3.jpg"/>
+<img style="width: 30%" src="static/new/columbus/4.jpg"/>
+<img style="width: 30%" src="static/new/columbus/5.jpg"/>
+<img style="width: 30%" src="static/new/columbus/6.jpg"/>
+<img style="width: 30%" src="static/new/columbus/7.jpg"/>
+<img style="width: 30%" src="static/new/columbus/8.jpg"/>
+<img style="width: 30%" src="static/new/columbus/9.jpg"/>
+
+<br/>
+<script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=QaA8hwLRMVimUf1kYj2bvyHZrHgyX0bi&width=500&height=400&lang=ru_RU&sourceType=constructor"></script>
--- web/place_rivera.php
+++ web/place_rivera.php
@@ -1,1 +1,5 @@
-Ривера
+<h1>Ривера</h1>
+Скоро открытие
+<br/>
+<br/>
+<script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=nJsyqQRRtbwiC5d2-HFc6I_yxh7SxO2R&width=500&height=400&lang=ru_RU&sourceType=constructor"></script>
