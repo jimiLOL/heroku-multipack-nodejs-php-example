cb443e07358bed8a77b865e5dded0be373785423
avdim <avdim@mail.ru>
2016-03-07 11:45:07 +0300

    map and photos

--- web/place_columbus.php
+++ web/place_columbus.php
@@ -1,29 +1,49 @@
 <h1>Columbus</h1>
-<p>
-    Детская развлекательная площадка Little Star расположена на 3 уровне ТЦ Columbus в непосредственной близости от станции метро Пражская.
-</p>
-<p>
-    Адрес: Москва, ул. Кировоградская, д. 13.<br/>
-    Работаем в режиме торгового центра.
-</p>
-<p>
-    Это идеальное место для малышей и зимой и летом: стремительные спуски с горок в бассейн, наполненный разноцветными шариками, катание на дельфинах перенесут в волшебный мир. Мир, в котором нет ничего невозможного. Мир, где сбываются все детские мечты!
-</p>
+<div style="text-align: justify">
+    <p>
+        Детская развлекательная площадка Little Star расположена на 3 уровне ТЦ Columbus в непосредственной близости от станции метро Пражская.
+    </p>
+    <p>
+        Адрес: Москва, ул. Кировоградская, д. 13.<br/>
+        Работаем в режиме торгового центра.
+    </p>
+    <p>
+        Это идеальное место для малышей и зимой и летом: стремительные спуски с горок в бассейн, наполненный разноцветными шариками, катание на дельфинах перенесут в волшебный мир. Мир, в котором нет ничего невозможного. Мир, где сбываются все детские мечты!
+    </p>
 
-<?php require_once "places_text_bottom.php"?>
-
-<a href="columbus_1.html">
-    <img style="width: 30%" src="static/new/columbus/1.jpg"/>
-</a>
-
-<img style="width: 30%" src="static/new/columbus/2.jpg"/>
-<img style="width: 30%" src="static/new/columbus/3.jpg"/>
-<img style="width: 30%" src="static/new/columbus/4.jpg"/>
-<img style="width: 30%" src="static/new/columbus/5.jpg"/>
-<img style="width: 30%" src="static/new/columbus/6.jpg"/>
-<img style="width: 30%" src="static/new/columbus/7.jpg"/>
-<img style="width: 30%" src="static/new/columbus/8.jpg"/>
-<img style="width: 30%" src="static/new/columbus/9.jpg"/>
-
+    <?php require_once "places_text_bottom.php"?>
+</div>
+<table style="width: 90%">
+    <tr>
+        <td style="width: 30%; vertical-align: middle">
+            <img src="static/columbus1.png" style="width: 100%"/>
+        </td>
+        <td style="width: 70%">
+            <script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=QaA8hwLRMVimUf1kYj2bvyHZrHgyX0bi&width=100%&height=400&lang=ru_RU&sourceType=constructor"></script>
+        </td>
+    </tr>
+</table>
 <br/>
-<script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=QaA8hwLRMVimUf1kYj2bvyHZrHgyX0bi&width=500&height=400&lang=ru_RU&sourceType=constructor"></script>
+<div id="photos">
+    <a href="columbus_1.html">
+        <img style="width: 30%" src="static/new/columbus/1.jpg"/>
+    </a>
+
+    <img style="width: 30%" src="static/new/columbus/2.jpg"/>
+    <img style="width: 30%" src="static/new/columbus/3.jpg"/>
+    <img style="width: 30%" src="static/new/columbus/5.jpg"/>
+    <img style="width: 30%" src="static/new/columbus/6.jpg"/>
+    <img style="width: 30%" src="static/new/columbus/7.jpg"/>
+    <table style="width: 66%">
+        <tr>
+            <td style="width: 53%; vertical-align: top;">
+                <img style="width: 100%" src="static/new/columbus/8.jpg"/>
+                <img style="width: 100%" src="static/new/columbus/9.jpg"/>
+            </td>
+            <td style="width: 1%"></td>
+            <td style="width: 46%">
+                <img style="width: 100%" src="static/new/columbus/4.jpg"/>
+            </td>
+        </tr>
+    </table>
+</div>
