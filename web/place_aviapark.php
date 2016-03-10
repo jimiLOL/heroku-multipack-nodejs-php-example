7848b6de40d82e0ec9e6e45cc24cd52e2ad1ea4d
avdim <avdim@mail.ru>
2016-03-10 10:21:51 +0300

    aviapark map rc2

--- web/place_aviapark.php
+++ web/place_aviapark.php
@@ -5,7 +5,7 @@
             <img src="static/aviapark1.png" style="width: 100%"/>
         </td>
         <td style="width: 70%">
-            <script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=D0cYdnOYWKyN_L3ROos4u0jg-2AFVBFt&width=100%&height=400&lang=ru_RU&sourceType=constructor"></script>
+            <script type="text/javascript" charset="utf-8" src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=D0cYdnOYWKyN_L3ROos4u0jg-2AFVBFt&width=100%&height=600&lang=ru_RU&sourceType=constructor"></script>
         </td>
     </tr>
 </table>
