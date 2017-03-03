a2d79c380683669a7fb648fca352b6526cf70eb2
dim <avdim@mail.ru>
2017-03-03 15:44:41 +0300

    remove aviapark video. add aviapark places pic

--- web/place_aviapark.php
+++ web/place_aviapark.php
@@ -53,4 +53,4 @@
         <img style="width: 30%" src="static/new/aviapark/small_8.jpg"/>
     </a>
 </div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/-SqyZwuey7s" frameborder="0" allowfullscreen></iframe>
+<!--iframe width="560" height="315" src="https://www.youtube.com/embed/-SqyZwuey7s" frameborder="0" allowfullscreen></iframe-->
